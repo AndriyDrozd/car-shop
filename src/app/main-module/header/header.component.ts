b0ff29ea6c19600dc25e87b4465232490a72bc89
AndriyDrozd <andriydrozdit@gmail.com>
2019-06-14 15:52:21 +0300

    Made responsive navbar

--- src/app/main-module/header/header.component.ts
+++ src/app/main-module/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  private toogleButton: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
